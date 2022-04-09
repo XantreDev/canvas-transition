055c0cb2ba2459f59efca5d262cb7e1d9936c9e9
XantreGodlike <valerik007.vs@gmail.com>
2022-04-09 16:51:21 +0300

    Animation fix

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // @ts-check
 
-import { drawCircles, drowBorder, drowLines, generateCoordinates, getContainer } from "./utils.js";
-import { setCanvasSize, getIntermidiate } from './utils.js';
+import { easeInOutBack } from "./easings.js";
+import { drawCircles, drowBorder, drowLines, generateCoordinates, getContainer, mergePoints } from "./utils.js";
+import { setCanvasSize, getIntermidiate, getRandom } from './utils.js';
 
 
 const canvas = document.getElementsByTagName("canvas")[0]
 
 const CONSTANTS = {
     CIRCLE_RADIUS: 10,
-    TRANSITION_DURATION: 250,
-    TRANSITION_FPS: 120
+    TRANSITION_DURATION: 600,
+    TRANSITION_FPS: 90
 }
 
 let transitonFinished = true
@@ -17,33 +18,51 @@ let transitionID = 0
 
 const transition = (previousPoints) => (canvas) => {
     const container = getContainer(window.innerHeight, window.innerWidth)
-    const newPoints = generateCoordinates(container, CONSTANTS.CIRCLE_RADIUS)
+    const pointCount = (() => {
+      let tmp = getRandom(2, 10) 
+      while(tmp === previousPoints.length) {
+        tmp = getRandom(2, 10)
+      }
+      return tmp
+    })()
+
+    const newPoints = generateCoordinates(container, pointCount)
         .map(([x, y]) => [x + CONSTANTS.CIRCLE_RADIUS, y])
 
-    const count = CONSTANTS.TRANSITION_DURATION / 1000 * CONSTANTS.TRANSITION_FPS
+    const frameCount = CONSTANTS.TRANSITION_DURATION / 1000 * CONSTANTS.TRANSITION_FPS
 
 
+    // @ts-ignore
+    const { newPoints: exacNewPoints, oldPoints } =  mergePoints(previousPoints, newPoints)
+    // const collapsedNew = exacNewPoints.filter((value, index, arr) => (
+    //   index === 0 || value[0] !== arr[index-1][0] || value[1] !== arr[index-1][0] 
+    // ))
+    console.log(newPoints, exacNewPoints)
 
-    const animate = (count, previousPoints, newPoints) => {
+
+    // const animatedPoints = []
+    // for (let item of previousPoints) {
+
+    // }
+    
+    const animate = (count, oldPoints, exacNewPoints) => {
         let i = 1
 
-        const easeInOut = (k) => .5*(Math.sin((k - .5)*Math.PI) + 1)
-        
         return () => {
             if (i >= count) {
-                draw(canvas, newPoints)
+                // draw(canvas, newPoints)
                 transitonFinished = true
                 return
             }
 
-            const intemediatePoints = getIntermidiate(previousPoints, newPoints, i / count, easeInOut)
+            const intemediatePoints = getIntermidiate(oldPoints, exacNewPoints, i / count, easeInOutBack)
             draw(canvas, intemediatePoints)
             i++
         }
     }
 
     transitonFinished = false
-    transitionID = setInterval(animate(count, previousPoints, newPoints), 1000 / CONSTANTS.TRANSITION_FPS)
+    transitionID = setInterval(animate(frameCount, oldPoints, exacNewPoints), 1000 / CONSTANTS.TRANSITION_FPS)
 
     return transition(newPoints);
 }
@@ -55,7 +74,7 @@ const draw = (canvas, prePoints = undefined) => {
     setCanvasSize(canvas, container.height, container.width)
     
     drowBorder(ctx, container, 'black')
-    const points = prePoints ?? generateCoordinates(container, CONSTANTS.CIRCLE_RADIUS)
+    const points = prePoints ?? generateCoordinates(container, 10)
     const accuratePoints = points.map(([x, y]) => [x + CONSTANTS.CIRCLE_RADIUS, y])
     
     drowLines(ctx, accuratePoints, 'black')
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 export const drowBorder = (ctx, container, color) => {
     ctx.strokeStyle = color;
     ctx.beginPath()
@@ -76,13 +78,199 @@ export const setCanvasSize = (canvas, height, width) => {
     canvas.height = height;
 }
 
-export const getIntermidiate = (previousPoints, newPoints, k, timing = (k) => k) => {
-    const intermidiatePoints = previousPoints.map(([x, oldY], index) => {
-        const [_, y] = newPoints[index]
-        const delta = y - oldY
+export const getIntermidiate = (previousPoints, newPoints, k, timing = (k) => k) => (
+  newPoints.map((newPoint, index) => {
+    const oldPoint = previousPoints[index]
 
-        return [x, oldY + delta * timing(k)]
+    return oldPoint.map((coordinate, index) => {
+      const newCoordinate = newPoint[index]
+      return coordinate + (newCoordinate - coordinate) * timing(k)
     })
+  })
+)
+// export const getIntermidiate = (previousPoints, newPoints, mergeResult, k, timing = (k) => k) => {
+//     const [newPointsPositions, oldPointsTargets, stretch] = mergeResult
 
-    return intermidiatePoints
+//     const originalPoints = stretch ? newPoints : previousPoints
+//     const mainPoints = !stretch ? newPoints : previousPoints
+//     const deltaSign = stretch ? -1 : 1
+//     console.log(mainPoints)
+//     console.log(originalPoints)
+//     console.log(newPointsPositions)
+//     console.log(oldPointsTargets)
+
+//     const intermidiatePoints = originalPoints
+//         .map(([originalX, originalY], index) => {
+//             if (newPointsPositions[index] === -1) return [originalX, originalY]
+//             const [x, y] = mainPoints[newPointsPositions[index]]
+//             const deltaY = deltaSign * (y - originalY)
+//             const deltaX = deltaSign * (x - originalX)
+
+//             return [originalX + deltaX * timing(k), originalY + deltaY * timing(k)]
+//         })
+//         .map(([originalX, originalY], index, array) => {
+//             if (oldPointsTargets[index] === -1) return [originalX, originalY]
+//             const [x, y] = array[newPointsPositions[oldPointsTargets[index]]]
+//             const deltaY = deltaSign * (y - originalY)
+//             const deltaX = deltaSign * (x - originalX)
+        
+//             const position = stretch ? 1 - timing(k) : timing(k)
+//             // const position = timing(k)
+            
+//             return [originalX + deltaX * position, originalY + deltaY * position]
+//         })
+//     return intermidiatePoints
+// }
+
+export const getRandom = (min, max, step = 1) => {
+    const delta = max - min
+
+    const randomNumber = Math.floor(Math.random() * (delta)) * step + min
+
+    return randomNumber
 }
+
+const getUniqueRandoms = (count, max) => {
+    const tmp = new Set()
+    while (tmp.size !== count) {
+        tmp.add(getRandom(0, max - 1))
+    }
+
+    return [...tmp].sort()
+}
+
+/**
+ * 
+ * @param {[number, number][][]} arrs 
+ */
+const sortOnLength = (arrs) => [...arrs].sort((a, b) => a.length - b.length)
+
+/**
+ * 
+ * @param {([number, number])[]} oldPoints 
+ * @param {([number, number])[]} newPoints 
+ */
+export const mergePoints = (oldPoints, newPoints) => {
+  const [lessPoints, morePoints] = sortOnLength([oldPoints, newPoints])
+  const xDeltas = lessPoints.map(([x]) => morePoints.map(([xM]) => Math.abs(xM - x)))
+  const xTargets = xDeltas.map(arr => arr.indexOf(Math.min(...arr)))
+
+  const tmp = Array(morePoints.length).fill()
+  // xTargets.forEach((targetIndex, index) => {
+  //   tmp[targetIndex] = lessPoints[index]
+  // })
+
+  let i = 0
+  const result = tmp.map((_, index) => {
+    if (index < xTargets[i])
+      return lessPoints[i]
+    else if (index === xTargets[i]) {
+      return lessPoints[i++]  
+    }
+    else {
+      return lessPoints.slice(-1)[0]
+    }
+  })
+
+  // console.log(morePoints, result)
+  // console.dir(result)
+  return {
+    oldPoints: oldPoints.length <= newPoints.length ? result : morePoints,
+    newPoints: oldPoints.length <= newPoints.length ? morePoints : result
+  }
+
+  // tmp.map((value, index) => {
+  //   if (xTargets[i] > index) {
+      
+  //   } else {
+  //     return value
+  //   }
+  // })
+  // const result = tmp.map((item, index) => {
+  //   if (item && typeof item[0] === 'number') {
+  //     return item
+  //   }
+
+    
+  //   const [xM] = morePoints[index]
+
+  // })
+
+    
+}
+
+// export const mergePoints = (oldPoints, newPoints) => {
+//     // if (previousPoints.length >= newPoints.length) {
+//     const [maximal, minimal] = oldPoints.length >= newPoints.length 
+//         ? [oldPoints.length, newPoints.length] 
+//         : [newPoints.length, oldPoints.length]
+    
+//     const targetIndexes = Array(maximal).fill(-1)
+//     // let selected = 0
+//     const newPointsPositions = getUniqueRandoms(minimal, maximal)
+    
+//     let lastIndex = 0
+//     newPointsPositions.forEach((value, index) => {
+//         targetIndexes[value] = lastIndex++
+//     })
+//     const firstNew = targetIndexes.findIndex((element) => element !== -1)
+//     const lastNew = targetIndexes.length - 1 - targetIndexes.reverse().findIndex((element) => element !== -1)
+//     console.log(firstNew, lastNew)
+//     targetIndexes.reverse()
+
+//     const oldPointsTargets = new Array(maximal).fill(-1).map((value, index) => {
+//         if (targetIndexes[index] !== -1)
+//         {
+//             return value
+//         }
+
+//         if (index < firstNew) {
+//             return firstNew
+//         }
+//         if (index > lastNew) {
+//             return lastNew
+//         }
+
+//         const leftOrRight = getRandom(-1, 1, 2) 
+//         // console.log(leftOrRight)
+
+//         let delta = leftOrRight
+//         while (targetIndexes[delta + index] === -1){
+//             delta += leftOrRight
+//         }
+
+//         return index + delta
+//     })
+
+//     return [targetIndexes, oldPointsTargets, maximal === newPoints.length ? true : false]
+
+//         // const targetIndexes = Array(oldPoints.length).fill(-1)
+//         // // let selected = 0
+//         // const newPointsPositions = getUniqueRandoms(newPoints.length, oldPoints.length)
+//         // newPointsPositions.forEach((value, index) => {
+//         //     targetIndexes[value] = value
+//         // })
+        
+//         // const oldPointsTargets = new Array(targetIndexes.length).fill(-1).map((value, index) => {
+//         //     if (newPointsPositions[index] !== -1)
+//         //     {
+//         //         return value
+//         //     }
+
+//         //     const leftOrRight = Math.floor(Math.random() * 2) - 1
+
+//         //     if (newPointsPositions[index + leftOrRight] !== undefined) {
+//         //         return index + leftOrRight
+//         //     }
+
+//         //     if (newPointsPositions[index + leftOrRight] === undefined) {
+//         //         return index - leftOrRight
+//         //     }
+//         // })
+
+//         // return [newPointsPositions, oldPointsTargets]
+//     // }
+
+//     // else {
+//     // }
+// }
